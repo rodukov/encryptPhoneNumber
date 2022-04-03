7a6b769e2bf04366ae1e5c699154dd8a1596f0f1
rodukov <electroneuphoria@gmail.com>
2022-04-03 16:37:21 +0300

    Commit

--- main.cpp
+++ main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <string>
 #include <list>
-#include <time.h>       /* time */
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 void printi(int text){ cout << text << endl; }
 void prints(string text){ cout << text << endl; }
 
+long int random(int to) {
+    srand((unsigned) time(0));
+    int RandomNumber = rand(); string output;
+    for(short int c = 0; c < to; c++){
+        output += to_string(RandomNumber)[c];
+    }
+    return stoi(output);
+}
+
 class encrypt_phone_number{
+    private:
+        long int pseudo_random_number = random(6);
     public:
         long int phonenumber;
         void dissolve() {
@@ -25,17 +37,20 @@ class encrypt_phone_number{
                     }
                 if(dissolvePhoneNumberHistory == dissolvePhoneNumber) { break; }
             }
-            cout << dissolvePhoneNumber << " * ";
+
+            dissolvePhoneNumber = pseudo_random_number*120000 - dissolvePhoneNumber;
+
+            cout << "( " << pseudo_random_number*120000 << " - " << dissolvePhoneNumber << " )" << " * ";
             for(auto v: numbers)
                 cout << v << " * ";
-            
+            cout << "1" << endl;
         }
 };
 
 int main() {
     encrypt_phone_number userphone;
-    userphone.phonenumber = 0; // your phone number here
+    userphone.phonenumber = 0; // Your phone number here
     userphone.dissolve();
-
     return 0;
+
 }
